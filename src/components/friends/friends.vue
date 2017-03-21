95a92b525e9d1c8d0ac1b9710277b6b647dc73ca
liulei6040 <784139247@qq.com>
2017-03-21 17:04:39 +0800

    friends

---
+++ src/components/friends/friends.vue
@@ -0,0 +1,25 @@
+<template>
+	<h1> recent!</h1>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+
+    },
+    data() {
+      return {
+
+      };
+    },
+    methods: {
+
+    },
+    components: {
+
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
