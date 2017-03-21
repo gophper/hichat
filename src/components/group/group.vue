e9fcb735173d37625422cc076b70a4c2bfc72040
liulei6040 <784139247@qq.com>
2017-03-21 20:04:38 +0800

    Hichat开发

---
+++ src/components/group/group.vue
@@ -0,0 +1,26 @@
+<template>
+	<h1> group!</h1>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+
+    },
+    data() {
+      return {
+
+      };
+    },
+    methods: {
+
+    },
+    components: {
+
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
