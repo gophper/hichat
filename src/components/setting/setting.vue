accf1d2b797eeb3ee16f0fd3e944e8770558b0af
liulei6040 <784139247@qq.com>
2017-03-21 18:27:58 +0800

    setting

---
+++ src/components/setting/setting.vue
@@ -0,0 +1,25 @@
+<template>
+	<h1> setting!</h1>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+
+    },
+    data() {
+      return {
+
+      };
+    },
+    methods: {
+
+    },
+    components: {
+
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
