cfe1e9f32ebbb2c3eca21a1d4a75123ecf779423
liulei6040 <784139247@qq.com>
2017-03-22 15:19:57 +0800

    friends

---
+++ src/components/friends/friendList.vue
@@ -0,0 +1,29 @@
+<template>
+	<div v-if="friendList" class="disable-user-behavior">
+		<div class="list">
+			<!--  好友列表-->
+			<portraitCard></portraitCard>
+		</div>
+	</div>
+</template>
+
+<script type="text/ecmascript-6">
+
+	import portraitCard from 'components/common/portraitCard';
+	export default {
+		props: {
+			friends:{
+			    type:'Array'
+			}
+		},
+		data() {
+			return {
+				friendList:[]
+			};
+		},
+		methods: {},
+		components: {
+			portraitCard
+		}
+	};
+</script>
