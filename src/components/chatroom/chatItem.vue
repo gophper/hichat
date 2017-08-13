ee0688cf86fe549c069750b4a7af9264edd9a196
liulei6040 <784139247@qq.com>
2017-08-13 23:12:47 +0800

    Merge branch 'master' of https://github.com/Lorenlei/Hichat
    	new file:   src/components/chatroom/chatItem.vue
    	new file:   src/components/friends/friendItem.vue
    	new file:   static/img/portrait/2017-06-06_22-12-433449c9e5e332f1dbb81505cd739fbf3f-53.png
    	new file:   static/img/portrait/2017-06-06_22-25-23-11111.png
    	new file:   static/img/portrait/2017-06-06_22-25-233449c9e5e332f1dbb81505cd739fbf3f-53.png
    	new file:   "static/img/portrait/360\345\217\215\351\246\210\346\204\217\350\247\201\346\210\252\345\233\276162412303564413449c9e5e332f1dbb81505cd739fbf3f-53.png"
    	new file:   static/img/portrait/center3449c9e5e332f1dbb81505cd739fbf3f-53.jpg
    	new file:   static/img/portrait/ionic3449c9e5e332f1dbb81505cd739fbf3f-53.png
    	new file:   static/img/portrait/thumbnail013449c9e5e332f1dbb81505cd739fbf3f-53.jpg
    	new file:   static/img/portrait/thumbnail023449c9e5e332f1dbb81505cd739fbf3f-53.jpg
    	new file:   static/img/portrait/thumbnail033449c9e5e332f1dbb81505cd739fbf3f-53.jpg
    	new file:   static/img/portrait/thumbnail043449c9e5e332f1dbb81505cd739fbf3f-53.jpg
    	new file:   static/img/portrait/user023449c9e5e332f1dbb81505cd739fbf3f-53.jpg

---
+++ src/components/chatroom/chatItem.vue
@@ -0,0 +1,56 @@
+<template>
+
+	<div class="list border-none">
+		<div class="item" v-bind:class="classObject">
+			<img  :src="item.sPorTrait">
+			<p class="chat-text ng-binding" v-bind:class="classObject2"> {{item.sContent}}</p>
+		</div>
+	</div>
+</template>
+
+<script type="text/ecmascript-6">
+	import goback from 'components/common/goback';
+	import popupInput from 'components/common/popup-input';
+	import store from 'common/js/store';
+	export default {
+		props: {
+		    item:Object
+		},
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+			showPromptAdd(){
+				this.$refs.popup.hide = false;
+			},
+			openSearch(){
+			}
+
+		},
+		components: {
+			popupInput,
+			goback
+		},
+		computed: {
+			classObject(){
+				if(this.item.iFromUserId  == store.get('info').iUserId){
+					return {'item-avatar-right-s':true, 'chat-me' :true,'text-right':true};
+				}else{
+					return {'item-avatar-s':true, 'chat-friend' :true};
+				}
+			},
+			classObject2(){
+				if(this.item.iFromUserId  == store.get('info').iUserId){
+					return {'positive-bg':true, 'text-left' :true};
+				}else{
+					return {'stable-bg':true};
+				}
+			}
+		},
+		mounted(){
+			console.log(this.item);
+		}
+	};
+</script>
